8711a0cebeb613176fcd9c926e406c3d1525bb6b
Matheus Rocha <matheusrochacamarg@gmail.com>
2025-05-28 22:26:51 -0300

    chore: add nativewind + tailwind config

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Stack } from "expo-router";
+import "../global.css";
+
+export default function _layout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,19 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import { StatusBar } from 'expo-status-bar';
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
 export default function index() {
   return (
     <View style={styles.container}>
-      <Text>Cefaly</Text>
-      <StatusBar style="auto" />
+      <Text className="">Cefaly</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

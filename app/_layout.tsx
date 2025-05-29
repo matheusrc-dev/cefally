a632fbedde3fc7e625cccec635fa2f159ba302a0
Matheus Rocha <matheusrochacamarg@gmail.com>
2025-05-28 23:28:39 -0300

    feat: add sign in and sign up screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 
 export default function _layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaView className="flex-1">
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeAreaView>
   );
 }

0115922060710b24771a5bf6d6060cdd3f85e5b4
Matheus Rocha <48885595+matheusrc-dev@users.noreply.github.com>
2025-05-29 00:34:21 -0300

    feat: add welcome screen (#14)

---
+++ app/(home)/welcome.tsx
@@ -0,0 +1,36 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import { router } from "expo-router";
+import { user } from "../../src/mocks/user";
+
+export default function welcome() {
+  const { name } = user;
+
+  function handleNewEpisode() {
+    router.push("/home");
+  }
+
+  return (
+    <View className="flex-1 items-center justify-center px-8 bg-white">
+      <Text className="text-9xl py-4 mb-6">👋</Text>
+
+      <Text className="text-3xl font-bold mb-4">Olá, {name}!</Text>
+
+      <Text className="text-center text-base mb-6">
+        Parabéns! você já sabe como é importante manter um registro das crises e estamos aqui para te ajudar.
+      </Text>
+
+      <Text className="text-center font-semibold mb-4">
+        Clique abaixo para registrar um episódio 👇
+      </Text>
+
+      <TouchableOpacity
+        onPress={handleNewEpisode}
+        className="w-full bg-cyan-800 rounded-md py-3 items-center"
+      >
+        <Text className="text-base font-semibold text-white">
+          Registrar novo episódio
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
